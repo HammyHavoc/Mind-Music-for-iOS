a8dc4975e1877c73231292c189e50561671c767e
Hammy Havoc <hammy@splitanatom.com>
2019-07-31 12:39:35 +0100

    Added push notification integration

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ define( function ( require ) {
 		wp_ws_url : 'https://www.mindmusic.online/wp-appkit-api/mind-music-ios',
 		wp_url : 'https://www.mindmusic.online',
 		theme : 'q-android-master',
-		version : '1.1',
+		version : '1.2',
 		app_type : 'phonegap-build',
 		app_title : 'Mind Music',
 		app_platform : 'ios',
@@ -15,9 +15,9 @@ define( function ( require ) {
 		gmt_offset : 0,
 		debug_mode : 'off',
 		auth_key : 'Q$#;h-|OlLx;G[V}qgL^,h?L]K95^u8QA2O#N6UaNE@/]~VrU4fb-.V]j|sI5`6-',
-		options : {"refresh_interval":0},
+		options : {"refresh_interval":0,"onesignal":{"app_id":"e5edb987-35f5-4ef7-9eb8-bd1e455a5a5d"}},
 		theme_settings : [],
-		addons : []
+		addons : [{"name":"OneSignal for WP AppKit","slug":"wpak-addon-onesignal","url":"https:\/\/www.mindmusic.online\/wp-content\/plugins\/wpak-addon-onesignal","js_files":[{"file":"js\/wpak-onesignal.js","type":"module","position":"","platforms":["android","ios"]},{"file":"js\/wpak-onesignal-app.js","type":"theme","position":"after","platforms":["android","ios"]}],"css_files":[],"html_files":[],"template_files":[],"app_data":null}]
 	};
 
 });
