2d8dba21db7e08552768303bfdeab7376be3c173
Hammy Havoc <hammy@splitanatom.com>
2019-07-31 11:02:19 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'mind-music-ios',
+		wp_ws_url : 'https://www.mindmusic.online/wp-appkit-api/mind-music-ios',
+		wp_url : 'https://www.mindmusic.online',
+		theme : 'q-android-master',
+		version : '1.0',
+		app_type : 'phonegap-build',
+		app_title : 'Mind Music',
+		app_platform : 'ios',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'Q$#;h-|OlLx;G[V}qgL^,h?L]K95^u8QA2O#N6UaNE@/]~VrU4fb-.V]j|sI5`6-',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
